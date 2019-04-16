30c3d8770f1cf1f5c69145d0c40cf44ad3fb324b
Marcio Junior Vieira <marcio@ambientelivre.com.br>
2019-04-16 19:53:33 -0300

    adicionado setup.php

---
+++ clonecomputer/setup.php
@@ -0,0 +1,13 @@
+<? 
+/**
+ * Nome e Versão do Plugin
+ */
+function plugin_version_clonecomputer() {
+   return array('name'           => "Clone Computer",
+                'version'        => '1.0.0',
+                'author'         => 'Davide Bizzarri',
+                'license'        => 'GPLv2+',
+                'homepage'       => '',
+                'minGlpiVersion' => '0.85');
+}
+
