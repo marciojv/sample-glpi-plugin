cb6d201990cae4359afa575b6275e2ac66020031
Marcio Junior Vieira <marcio@ambientelivre.com.br>
2019-04-16 20:07:56 -0300

    adicionado hook.php

---
+++ clonecomputer/hook.php
@@ -0,0 +1,11 @@
+<? 
+ 
+/**
+ * Chamado quando usuario clica em instalar
+ */
+function plugin_clonecomputer_install() { return true; }
+ 
+/**
+ * Chamado quando usuario clica em desinstalar
+ */
+function plugin_clonecomputer_uninstall() { return true; }
